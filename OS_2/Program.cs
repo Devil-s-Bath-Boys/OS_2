6d8e6cc21a7d7c42ebb51f8d97237ae2aba1c679
Devil-s-Bath-Boys <warpugg@gmail.com>
2022-04-04 01:55:25 +0300

    UPDATED

--- OS_2/Program.cs
+++ OS_2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
-using System.Diagnostics;
 using System.Threading;
 
 
@@ -9,7 +9,10 @@ namespace OS_2
 {
     internal class Program
     {
-        static string[] initialHashes = { "1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad", "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b", "74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f" };
+        static bool trigger = false;
+        static string initialHashes1 = "1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad";
+        static string initialHashes2 = "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b";
+        static string initialHashes3 = "74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f";
         static int size = 11881376;
         static string alphabet = "abcdefghijklmnopqrstuvwxyz";
         static char[] hashDecrypt = new char[5];
@@ -65,81 +68,99 @@ namespace OS_2
             return decryption;
         }
 
-        void divisionOfLabor(int start, int finish, string[] decryptedPassPull, int decryptedPassPullMarker)
+        static string divisionOfLabor(int start, int finish, string incomeHash)
         {
             string decryption = "";
 
-            for (int initialHashesMarker = 0; initialHashesMarker < 3; initialHashesMarker++)
+            for (int counter = start; counter < finish; counter++)
             {
-                for (int counter = start; counter < finish; counter++)
+                if (trigger == true)
                 {
-                    using (SHA256 sha256Hash = SHA256.Create())
-                    {
-                        byte[] sourceBytes = Encoding.UTF8.GetBytes(passPull[counter]);
-                        byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
-                        string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty).ToLower();
-
-                        if (hash == initialHashes[initialHashesMarker])
-                        {
-                            decryption = passPull[counter];
-                            decryptedPassPull[decryptedPassPullMarker] = decryption;
-                            decryptedPassPullMarker++;
-
-                            break;
-                        }
-                    }
-
+                    break;
                 }
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                    byte[] sourceBytes = Encoding.UTF8.GetBytes(passPull[counter]);
+                    byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+                    string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty).ToLower();
+
+                    if (hash == incomeHash)
+                    {
+                        decryption = passPull[counter];
+                        trigger = true;
+                        break;
+                    }
+                }
+
             }
 
+            return decryption;
         }
-        static void Second()
+
+        static string Second(string initialHash)
         {
-            Stopwatch swatch = new Stopwatch();
-            swatch.Start();
-            string[] decryptedPassPull = new string[3];
-            for (int initialHashesMarker = 0; initialHashesMarker < 3; initialHashesMarker++)
-            {
-                decryptedPassPull[initialHashesMarker] = passHashing(initialHashes[initialHashesMarker]);
-            }
-            Console.WriteLine("\n" + "Пароль, соответсвтующий первому хешу: " + decryptedPassPull[0] + "\n" +
-                              "Пароль, соответсвтующий второму хешу: " + decryptedPassPull[1] + "\n" +
-                              "Пароль, соответсвтующий третьему хешу: " + decryptedPassPull[2]);
-            swatch.Stop();
-            Console.WriteLine("\n" + "Время перебора в однопоточном режиме: " + swatch.Elapsed);
+            return passHashing(initialHash);
         }
-        /*
-                void Third()
+
+        static string Third(string incomeHash, int threadsAmount)
+        {
+            int difference = size / threadsAmount;
+            int remains = size % threadsAmount;
+            int start = 0;
+            int finish = 0;
+            string trueSaveHouse = "";
+
+            int[] iteration = new int[threadsAmount];
+
+            Thread[] threads = new Thread[threadsAmount];
+
+            for (int counter = 0; counter < threadsAmount; counter++)
+            {
+                iteration[counter] = difference;
+
+                if (remains != 0)
                 {
-                    Console.WriteLine("Введите количество потоков:\n");
-                    int threadsAmount = int.Parse(Console.ReadLine());
-
-                    int difference = size / threadsAmount;
-                    int start = 0;
-                    int finish = difference;
-
-                    string[] decryptedPassPull = new string[3];
-                    int decryptedPassPullMarker = 0;
-
-                    for (int threadsCounter = 0; threadsCounter < threadsAmount; threadsCounter++)
-                    {
-                        ThreadStart ts = new ThreadStart();
-                        Thread t = new Thread(ts);
-                        t.Start();
-                        start = start + difference;
-                        finish = finish + difference;
-
-                    }
-
+                    iteration[counter]++;
+                    remains--;
                 }
-        */
+
+                if (counter != 0) start = finish;
+                finish = start + iteration[counter];
+
+                int from = start;
+                int to = finish;
+
+                threads[counter] = new Thread(() =>
+                {
+                    string saveHouse = System.String.Empty;
+                    saveHouse = divisionOfLabor(from, to, incomeHash);
+                    if (!String.IsNullOrEmpty(saveHouse)) trueSaveHouse = saveHouse;
+                });
+                threads[counter].Start();
+            }
+
+            for (int counter = 0; counter < threadsAmount; counter++)
+            {
+                threads[counter].Join();
+                threads[counter].Interrupt();
+            }
+
+            start = 0;
+            finish = 0;
+            trigger = false;
+
+            return trueSaveHouse;
+        }
+
+
 
         static void Main(string[] args)
         {
+            int marker = 0;
             passPullForming();
             while (true)
             {
-                Console.WriteLine("Выход из программы - '1'.\n" +
+                Console.WriteLine("\n" + "Выход из программы - '1'.\n" +
                                   "Однопоточный режим работы - '2'.\n" +
                                   "Многопоточный режим - '3'.");
 
@@ -151,11 +172,29 @@ namespace OS_2
                         break;
 
                     case '2':
-                        Second();
+                        Stopwatch swatchSecond = new Stopwatch();
+                        swatchSecond.Start();
+                        Console.WriteLine("\n" + "Пароль, соответсвтующий первому хешу: " + Second(initialHashes1) +
+                                          "\n" + "Пароль, соответсвтующий второму хешу: " + Second(initialHashes2) +
+                                          "\n" + "Пароль, соответсвтующий третьему хешу: " + Second(initialHashes3));
+
+                        swatchSecond.Stop();
+                        Console.WriteLine("\n" + "Время перебора в однопоточном режиме: " + swatchSecond.Elapsed);
                         break;
 
                     case '3':
-                        //Third();
+                        Stopwatch swatchThird = new Stopwatch();
+                        swatchThird.Start();
+
+                        Console.WriteLine("\nВведите количество потоков:\n");
+                        int threadsAmount = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\n" + "Пароль, соответсвтующий первому хешу: " + Third(initialHashes1, threadsAmount));
+                        Console.WriteLine("\n" + "Пароль, соответсвтующий второму хешу: " + Third(initialHashes2, threadsAmount));
+                        Console.WriteLine("\n" + "Пароль, соответсвтующий третьему хешу: " + Third(initialHashes3, threadsAmount));
+
+                        swatchThird.Stop();
+                        Console.WriteLine("\n" + "Время перебора в многопоточном режиме: " + swatchThird.Elapsed);
                         break;
                     default:
                         {
@@ -165,12 +204,33 @@ namespace OS_2
 
                 }
 
-            }
+                while (marker == 0)
+                {
+                    Console.WriteLine("\n" + "Желаете отчистить консоль? y / n");
+                    char answer = Console.ReadKey().KeyChar;
 
+                    switch (answer)
+                    {
+                        case 'y':
+                            Console.Clear();
+                            marker = 1;
+                            break;
+
+                        case 'n':
+                            marker = 1;
+                            break;
+                        default:
+                            {
+                                Console.WriteLine("\n" + "Пожалуйста, введите y / n.\n");
+                                continue;
+                            }
+                    }
+                }
+                marker = 0;
+            }
         }
     }
 }
-
 
 
 
